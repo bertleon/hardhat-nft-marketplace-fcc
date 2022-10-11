6477ac595c574b2bae3beb19d088c4d76f9fafe9
Roberto Leon-Luna <rsleonluna>
2022-10-11 16:50:23 -0400

    finished moralis tutorial

---
+++ scripts/mint.js
@@ -0,0 +1,24 @@
+const { ethers, network } = require("hardhat")
+
+const { moveBlocks } = require("../utils/move-blocks")
+
+async function mint() {
+    const basicNft = await ethers.getContract("BasicNft")
+    console.log("Minting...")
+    const mintTx = await basicNft.mintNft()
+    const mintTxReceipt = await mintTx.wait(1)
+    const tokenId = mintTxReceipt.events[0].args.tokenId
+    console.log(`Got tokenId: ${tokenId}`)
+    console.log(`NFT Address: ${basicNft.address}`)
+
+    if (network.config.chainId == "31337") {
+        await moveBlocks(2, (sleepAmount = 1000))
+    }
+}
+
+mint()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.log(error)
+        process.exit(1)
+    })
